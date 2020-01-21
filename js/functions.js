cb30af784c69c37613b09e4652328b4e9707ed93
cefirox <luisbcn19@gmail.com>
2020-01-21 18:20:51 +0100

    update headers

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 	
-	var key = "RGAPI-c898e6b3-9c88-4c37-abdc-5e1fcb53af26";	
 	$("#btnMatchId").click(function(){
 		$("#displayData ul").html("");
 
+		var key = $("#key").val();
 		var matchId = $("#matchId").val();
 		var region = $("#region").val();
 		
 		var url = "https://"+region+".api.riotgames.com/lol/match/v4/matches/"+matchId+"?api_key="+key;
-		/*
+		
 		$.ajaxSetup({
 			headers : {
-			    "Origin": "*",
+				"crossDomain" : "true",
+			    "Origin": "https://developer.riotgames.com",
 			    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-			    "X-Riot-Token": key,
 			    "Accept-Language": "es,en;q=0.9,ca;q=0.8,gl;q=0.7",
+			    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
 			}
 		});
-		*/
-
+		
 		$.getJSON(url)
 		.done(function(WsData){
-			$("#displayData ul").append("<li>"+url+"</li>");
+			$("#displayData ul").append("<li>"+url+"?api_key="+key+"</li>");
 			$.each(WsData, function(index, value){
 				$("#displayData ul").append("<li>"+index+": "+value+"</li>");
 			});
@@ -29,15 +29,15 @@ $(document).ready(function(){
 			$("#displayData").show();
 		})
 		.fail(function(WsData, textStatus, errorThrown ) {
-			$("#displayData ul").append("<li>"+url+"</li>");
-            $.each(WsData, function(index, value){
+			$("#displayData ul").append("<li>"+url+"?api_key="+key+"</li>");
+	        $.each(WsData, function(index, value){
 			$("#displayData ul").append("<li>"+index+": "+value+"</li>");
 		});
 
-			$("#displayData").show();
+		$("#displayData").show();
 	        
-    	});
-		
+		});
+
 	});
 
 });
