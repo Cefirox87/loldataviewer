2bb8b04bc846442c06564d11a739b113ad869da2
cefirox <luisbcn19@gmail.com>
2020-01-22 12:41:04 +0100

    iframe test

--- js/functions.js
+++ js/functions.js
@@ -8,37 +8,26 @@ $(document).ready(function(){
 		var region = $("#region").val();
 		
 		var url = "https://"+region+".api.riotgames.com/lol/match/v4/matches/"+matchId+"?api_key="+key;
-		
-		$.ajaxSetup({
-			headers : {
-				"crossDomain" : "true",
-			    "Origin": "https://developer.riotgames.com",
-			    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-			    "Accept-Language": "es,en;q=0.9,ca;q=0.8,gl;q=0.7",
-			    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
-			}
-		});
-		
-		$.getJSON(url)
-		.done(function(WsData){
-			$("#displayData ul").append("<li>"+url+"?api_key="+key+"</li>");
-			$.each(WsData, function(index, value){
-				$("#displayData ul").append("<li>"+index+": "+value+"</li>");
-			});
 
-			$("#displayData").show();
-		})
-		.fail(function(WsData, textStatus, errorThrown ) {
-			$("#displayData ul").append("<li>"+url+"?api_key="+key+"</li>");
-	        $.each(WsData, function(index, value){
-			$("#displayData ul").append("<li>"+index+": "+value+"</li>");
-		});
+		var iframe = window.open(url, "jsoniframe");
 
-		$("#displayData").show();
-	        
-		});
+		$("#displayData").html("Loading...");	
+
+
+		setTimeout(function(){ 
+			//var data = $("#jsoniframe").contents(); 
+			
+			var data = document.getElementById('jsoniframe').contentWindow.document.body.innerHTML;
+			//var json = JSON.parse(JSON.stringify(data))
+			console.log(data)
+			//$.each(data, function(index, value){
+			//	$("#displayData ul").append("<li>"+index+": "+value+"</li>");
+			//});
+			//console.log(json)	
+		}, 2000);
+		
+		$("#displayData").show();   
 
 	});
 
 });
-
